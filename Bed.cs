69182a9499f1a060bf254b845de27bfb16e6ce44
Filip Statkiewicz <filip.statkiewicz25@gmail.com>
2022-11-06 16:09:24 +0100

    Initialize new project

---
+++ Bed.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Artisan
+{
+    class Bed
+    {
+        private int width;
+        private int footersHeight;
+        private int mattress;
+
+        public Bed(int getWidth, int getfootersHeight, int getMattress)
+        {
+            width = getWidth;
+            footersHeight = getfootersHeight;
+            mattress = getMattress;
+
+        }
+    }
+
+    
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Artisan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string item;
+            bool boolflag;
+
+            do
+            {
+                boolflag = true;
+                Console.Write("What would you like to sculpt? bed | bookshelf | chair : ");
+                item = Console.ReadLine();
+                item = item.ToLower();
+
+                if (item != null)
+                    if (item == "bed")
+                    {
+                        Bed createdItem = new Bed();
+                    }
+                    else if (item == "bookshelf")
+                    {
+                        Bookshelf createdItem = new Bookshelf();
+                    }
+                    else if (item == "chair")
+                    {
+                        Chair createdElement = new Chair();
+                    }
+                    else
+                    {
+                        Console.WriteLine("I'm sorry but I can't make sculpt this for you");
+                        boolflag = false;
+                    }
+            }
+            while (boolflag == false);
+
+            Console.WriteLine("Out of loop");
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
